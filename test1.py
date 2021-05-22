fd022e43a0ed9cb8b0de987f799c0d49f510b540
pangoro24 <p.gonzalez7027@gmail.com>
2021-05-22 10:32:35 -0500

    first commit

---
+++ test1.py
@@ -0,0 +1,33 @@
+import time
+
+
+
+
+
+
+def eladio_fnc(num1, num2):
+    pass
+    print("variables",num1,num2)
+    z=num1+num2
+    return z
+
+
+def farid_fnc(a,b): 
+    print("numeros:",a,b)
+    x=a+b
+    #print("Suma",x)
+    return x
+
+
+def jon_fnc(a,b):
+    print("numeros:",a,b)
+    x=a+b
+    #print("Suma",x)
+    return x
+    
+    
+    
+    
+
+print(farid_fnc(10,5))
+print(eladio_fnc(-3,-5))
