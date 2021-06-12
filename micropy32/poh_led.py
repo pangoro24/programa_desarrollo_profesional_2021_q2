0c4c074ef475e7dbaa3c250da085104847aac5f3
jonathanGG04 <jonathangonzalez63.jg@gmail.com>
2021-06-12 09:41:03 -0500

    nuevo

--- micropy32/poh_led.py
+++ micropy32/poh_led.py
@@ -1,6 +1,41 @@
-import time 
-import machine
-def parpadeo ( pin):
-    led = machine.Pin(pin,machine.Pin.OUT)
+import logging
+from machine  import Pin as DO
 
+class Light:
+    def __init__(self, pin):
+        """Inicializacion de un led
+        Parameters
+        ----------
+        pin : int
+            gpio, not board
+        """
+        self.pin = pin
+        self.light = DO(self.pin,Pin.OUT)
+        logging.debug("Led was set at pin# " + str(self.pin))
+
+    async def TurnOn(self):
+        self.light.on()
+        logging.debug("Led_{} turned on".format(self.pin))
+
+    async def TurnOff(self):
+        self.light.off()
+        logging.debug("Led_{} turned off".format(self.pin))
+        
+    async def CheckStatus(self):
+        return self.light.is_active
+
+    def TurnOnSync(self):
+        self.light.on()
+        logging.debug("Led_{} turned on".format(self.pin))
+
+    def TurnOffSync(self):
+        self.light.off()
+        logging.debug("Led_{} turned off".format(self.pin))
+    
+    def ToggleSync(self):
+        self.light.value()
+        return self.light.is_active
+    
+    def CheckStatusSync(self):
+        return self.light.is_active
 
