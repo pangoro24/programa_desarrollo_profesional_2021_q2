5ad52e35b56d26eb25b5c6941a657534028a0472
Eladio J <eladiojose20@gmail.com>
2021-06-12 10:57:08 -0500

    bt en main

--- micropy32/main.py
+++ micropy32/main.py
@@ -1,6 +1,8 @@
 from poh_led import Light
+from poh_btn import Bouton
 import time
 #import logging
+bt1= Bouton(4)
 
 led1 = Light(5)
 led1.TurnOnSync()
@@ -8,3 +10,12 @@ print(led1.CheckStatusSync())
 time.sleep(1)
 led1.ToggleSync()
 print("Done")
+
+while True:
+    bt1=bouton.value()
+    if not bt1:
+        led1.value(1)
+    else:
+        led1.value(0)
+    print(bt1)
+    time.sleep(1)
